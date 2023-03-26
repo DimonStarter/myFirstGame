eb41d4a78523cd12eeaf14d4120a909e240f233d
Dimon <gorokhov.diman@mail.ru>
2023-03-26 19:57:03 +0300

    init

--- cardGame/loseScreen.ts
+++ cardGame/loseScreen.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable no-undef */
 function loseBlock(container: Element) {
     const buttonToStart = document.createElement("button");
